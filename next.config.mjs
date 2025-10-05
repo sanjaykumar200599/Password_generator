150e40108bf27887da43b8406b7367f8dd84b378
Sanjay Kumar <sanjaykumar200599@gmail.com>
2025-10-05 21:35:04 +0530

    modified the dependencies

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+// next.config.mjs
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  // You can add other Next.js configuration options here if needed.
+};
+
+export default nextConfig;
